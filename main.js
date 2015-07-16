dd4db6f9916dbb63b4f8500e215346ae4957a6ff
Demo User <demouser@MacBook-Pro.local>
2015-07-15 17:23:47 -0700

    added js, pictures, and links to places on page

---
+++ main.js
@@ -0,0 +1,11 @@
+$('#kristie').mouseover(function(){
+  $(this).fadeOut().fadeIn();
+});
+
+$('#me').mouseover(function(){
+  $(this).fadeOut().fadeIn();
+});
+
+$('#carrie').mouseover(function(){
+  $(this).fadeOut().fadeIn();
+});
